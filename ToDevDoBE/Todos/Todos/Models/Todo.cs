9ba367dcfea3de2a8e9c67f5b3fdf41a3a7ba014
Cindel Sousa <60244854+Cindelevelyn@users.noreply.github.com>
2022-10-07 23:48:25 -0300

    Add files via upload

---
+++ ToDevDoBE/Todos/Todos/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿namespace Todos.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Descricao { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public bool Completed { get; set; }
+
+    }
+}
---
+++ ToDevDoBE/Todos/Todos/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using Todos.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseSqlServer(connectionString));
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(MyAllowSpecificOrigins,
+                          builder =>
+                          {
+                              builder.WithOrigins("http://localhost:8000/")
+                                                  .AllowAnyHeader()
+                                                  .AllowAnyMethod();
+                          });
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "ToDevDoAPI", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
